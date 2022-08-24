22bb73d19628eeb83993ad634179950bb768b853
LotsOfBlochs <69881693+LotsOfBlochs@users.noreply.github.com>
2022-08-24 15:18:27 -0700

    fix reversal bug.

--- src/Binary.cpp
+++ src/Binary.cpp
@@ -4,13 +4,12 @@ using namespace Obfuscate;
 
 std::string Obfuscator::Binary(std::string message) {
 	std::string newMessage;
-	for (int i = message.length()-1; i >= 0; i--) {
-		int n = message[i];
-		for (int i = 0; i < 8; i++)
+	for (char t : message) {
+		for (int n = 0; n < 8; n++)
 		{
-			char currentChar = (char)(48 + (n % 2));
+			char currentChar = (char)('0' + (t % 2));
 			newMessage.append(1,currentChar);
-			n = n / 2;
+            t /= 2;
 		}
 		newMessage.append((std::string)"\n");
 	}
@@ -19,10 +18,9 @@ std::string Obfuscator::Binary(std::string message) {
 }
 
 std::string Obfuscator::ReverseBinary(std::string message) {
-	std::string newMessage = message;
-	std::string newNewMessage;
-	for (long long i = (message.length() / 9)-1; i >= 0; i--) {
-		std::string currentBin = newMessage.substr(i * (long long)9, (i * (long long)9) + (long long)8);
+	std::string newMessage;
+	for (long long i = 0; i < message.length(); i+=9) {
+		std::string currentBin = message.substr(i, i + (long long)8);
 		char* currentCharBin = (char*)currentBin.c_str();
 		int currentChar = 1 * (currentCharBin[0] - 48);
 		currentChar += 2 * (currentCharBin[1] - 48);
@@ -33,10 +31,8 @@ std::string Obfuscator::ReverseBinary(std::string message) {
 		currentChar += 64 * (currentCharBin[6] - 48);
 		currentChar += 128 * (currentCharBin[7] - 48);
 		char currentCharecter = (char)currentChar;
-		newNewMessage.push_back(currentCharecter);
+        newMessage.push_back(currentCharecter);
 	}
-
-	static std::string finalMessage(newNewMessage.begin(), newNewMessage.end());
-	return finalMessage;
+	return newMessage;
 }
 
